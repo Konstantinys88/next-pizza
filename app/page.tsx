367f2f62956c3153e91dbf67ad5e8536c3941ab1
Maschkov Konstantin <maschkovc@yandex.ru>
2024-09-23 21:39:01 +0300

    add top bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,15 @@
+import { Container, Title, TopBar } from '@/components/ui/shared';
+
 export default function Home() {
-    return <h1>Главная</h1>;
+    return (
+        <>
+            <Container className='mt-10'>
+                <Title text='Все пиццы' size='lg' className='font-extrabold' />
+            </Container>
+            <TopBar />
+        </>
+    );
 }
 
-//50.30
+//50:30
+//1:03:06 - 23.09.2024
